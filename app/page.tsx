17a6d4a262cd817670cb28f4ede54137e1334d3c
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2024-11-16 17:04:00 -0300

    feat: add navigation bar component and integrate it into home and transactions pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { UserButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import NavBar from "./_components/navbar";
 
 export default async function Home() {
   const { userId } = await auth();
   if (!userId) {
     redirect("/login");
   }
-  return <UserButton showName />;
+  return <NavBar />;
 }
