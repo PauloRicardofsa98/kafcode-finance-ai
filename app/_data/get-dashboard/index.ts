28668ad3ec93e793896dd998312b50dc5032fbcb
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2024-11-17 13:52:08 -0300

    feat: add last transactions component

--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -96,6 +96,14 @@ export const getDashboard = async (month: string) => {
     ),
   }));
 
+  const lastTransactions = await db.transaction.findMany({
+    where,
+    orderBy: {
+      date: "desc",
+    },
+    take: 10,
+  });
+
   return {
     balance,
     depositsTotal,
@@ -103,5 +111,6 @@ export const getDashboard = async (month: string) => {
     expensesTotal,
     typesPercentages,
     totalExpensePerCategory,
+    lastTransactions,
   };
 };
