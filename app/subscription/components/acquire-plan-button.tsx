c12f4e078b9cc44108f821724de6131a31c10c79
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2024-11-17 16:42:18 -0300

    feat: add Stripe webhook handling and update subscription management logic

--- app/subscription/components/acquire-plan-button.tsx
+++ app/subscription/components/acquire-plan-button.tsx
@@ -2,8 +2,12 @@
 import { Button } from "@/app/_components/ui/button";
 import { createStripeCheckOut } from "../_actions/create-check-out";
 import { loadStripe } from "@stripe/stripe-js";
+import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 const AcquirePlanButton = () => {
+  const { user } = useUser();
+
   const handleAcquirePlanClick = async () => {
     const { sessionId } = await createStripeCheckOut();
 
@@ -21,12 +25,31 @@ const AcquirePlanButton = () => {
 
     await stripe.redirectToCheckout({ sessionId });
   };
+
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan === "premium";
+
+  if (hasPremiumPlan) {
+    return (
+      <Button
+        className="w-full rounded-full font-bold"
+        variant={"link"}
+        asChild
+      >
+        <Link
+          href={`${process.env.NEXT_PUBLIC_STRIPE_CUSTOMER_PORTAL_URL as string}?prefilled_email=${user.emailAddresses[0].emailAddress}`}
+        >
+          Gerenciar Plano
+        </Link>
+      </Button>
+    );
+  }
+
   return (
     <Button
       className="w-full rounded-full font-bold"
       onClick={handleAcquirePlanClick}
     >
-      Adquirir plano
+      Adquirir Plano
     </Button>
   );
 };
--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,9 +1,10 @@
-import { auth } from "@clerk/nextjs/server";
+import { auth, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import NavBar from "../_components/navbar";
 import { Card, CardContent, CardHeader } from "../_components/ui/card";
 import { CheckIcon, XIcon } from "lucide-react";
 import AcquirePlanButton from "./components/acquire-plan-button";
+import { Badge } from "../_components/ui/badge";
 
 const SubscriptionPage = async () => {
   const { userId } = await auth();
@@ -11,56 +12,71 @@ const SubscriptionPage = async () => {
     redirect("/login");
   }
 
+  const clientClerk = await clerkClient();
+  const user = await clientClerk.users.getUser(userId);
+
+  const hasPremiumPlan = user.publicMetadata.subscriptionPlan === "premium";
+
   return (
     <>
       <NavBar />
+      <div className="space-y-6 p-6">
+        <h1 className="text-2xl font-bold">Assinatura</h1>
 
-      <div className="flex gap-6">
-        <Card className="w-[450px]">
-          <CardHeader className="border-b border-solid py-8">
-            <h2 className="text-center text-2xl font-semibold">Plano Básico</h2>
-            <div className="flex items-center justify-center gap-3">
-              <span className="text-4xl">R$</span>
-              <span className="text-6xl font-semibold">0</span>
-              <div className="text-2xl text-muted-foreground">/mês</div>
-            </div>
-          </CardHeader>
-          <CardContent className="space-y-6 py-8">
-            <div className="flex items-center gap-2">
-              <CheckIcon className="text-primary" />
-              <p>Apenas 10 transações por mês</p>
-            </div>
-            <div className="flex items-center gap-2">
-              <XIcon />
-              <p>Relatório de IA</p>
-            </div>
-          </CardContent>
-        </Card>
+        <div className="flex gap-6">
+          <Card className="w-[450px]">
+            <CardHeader className="relative border-b border-solid py-8">
+              <h2 className="text-center text-2xl font-semibold">
+                Plano Básico
+              </h2>
+              <div className="flex items-center justify-center gap-3">
+                <span className="text-4xl">R$</span>
+                <span className="text-6xl font-semibold">0</span>
+                <div className="text-2xl text-muted-foreground">/mês</div>
+              </div>
+            </CardHeader>
+            <CardContent className="space-y-6 py-8">
+              <div className="flex items-center gap-2">
+                <CheckIcon className="text-primary" />
+                <p>Apenas 10 transações por mês</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <XIcon />
+                <p>Relatório de IA</p>
+              </div>
+            </CardContent>
+          </Card>
 
-        <Card className="w-[450px]">
-          <CardHeader className="border-b border-solid py-8">
-            <h2 className="text-center text-2xl font-semibold">
-              Plano Premium
-            </h2>
-            <div className="flex items-center justify-center gap-3">
-              <span className="text-4xl">R$</span>
-              <span className="text-6xl font-semibold">19</span>
-              <div className="text-2xl text-muted-foreground">/mês</div>
-            </div>
-          </CardHeader>
-          <CardContent className="space-y-6 py-8">
-            <div className="flex items-center gap-2">
-              <CheckIcon className="text-primary" />
-              <p>Transações ilimitadas</p>
-            </div>
-            <div className="flex items-center gap-2">
-              <CheckIcon className="text-primary" />
-              <p>Relatório de IA</p>
-            </div>
+          <Card className="w-[450px]">
+            <CardHeader className="relative border-b border-solid py-8">
+              {hasPremiumPlan && (
+                <Badge className="absolute left-4 top-4 bg-primary/10 text-primary">
+                  Ativo
+                </Badge>
+              )}
+              <h2 className="text-center text-2xl font-semibold">
+                Plano Premium
+              </h2>
+              <div className="flex items-center justify-center gap-3">
+                <span className="text-4xl">R$</span>
+                <span className="text-6xl font-semibold">19</span>
+                <div className="text-2xl text-muted-foreground">/mês</div>
+              </div>
+            </CardHeader>
+            <CardContent className="space-y-6 py-8">
+              <div className="flex items-center gap-2">
+                <CheckIcon className="text-primary" />
+                <p>Transações ilimitadas</p>
+              </div>
+              <div className="flex items-center gap-2">
+                <CheckIcon className="text-primary" />
+                <p>Relatório de IA</p>
+              </div>
 
-            <AcquirePlanButton />
-          </CardContent>
-        </Card>
+              <AcquirePlanButton />
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </>
   );
